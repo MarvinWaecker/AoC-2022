2c3d5b193dce580aa3f5bfccc10640a17905fc3f
CodeKalb <marvin.waecker@icloud.com>
2022-12-03 10:03:48 +0100

    day3 part2

--- day_3/day3.py
+++ day_3/day3.py
@@ -1,13 +1,21 @@
 import os
 
 def part1():
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_data.txt'), "r") as file:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data.txt'), "r") as file:
         s = 0
         for rs in file.read().splitlines():
             c = list(set(rs[:int(len(rs)/2)]).intersection(set(rs[int(len(rs)/2):])))
             s += ord(c[0].lower()) - 96 + 26 if c[0].isupper() else ord(c[0]) - 96
         return s
 
-
+def part2():
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data.txt'), "r") as file:
+        s = 0
+        rss = file.read().splitlines()
+        for i in range(2, len(rss), 3):
+            c = list(set.intersection(*[set(rss[i-2]), set(rss[i-1]), set(rss[i])]))
+            s += ord(c[0].lower()) - 96 + 26 if c[0].isupper() else ord(c[0]) - 96
+        return s
 
 print(part1())
+print(part2())
