6286b6d1fc60fd2adde456840cfc7ebd2e7ff86a
CodeKalb <marvin.waecker@icloud.com>
2022-12-04 11:05:13 +0100

    day4 part2

--- day_4/day4.py
+++ day_4/day4.py
@@ -1,16 +1,19 @@
 import os
 
-def part1():
+def part1and2():
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data.txt'), "r") as file:
-        score = 0
+        score1 = 0
+        score2 = 0
         for line in file.read().splitlines():
             pair = ([list(map(int, sec.split('-'))) for sec in line.split(',')]) 
             for i, s in enumerate(pair):
                 pair[i] = set(range(s[0], s[1] + 1))
             if pair[0].issubset(pair[1]) or pair[1].issubset(pair[0]):
-                score += 1
-        return score
+                score1 += 1
+            if bool(set(pair[0]) & set(pair[1])):
+                score2 += 1
+        return score1, score2
 
 
-print(part1())
+print(part1and2())
 
